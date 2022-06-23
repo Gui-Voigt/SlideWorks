dc1b8a64e9678257b77a3599b388b5e0ca5d661f
Gui-Voigt <guilherme.voigt@outlook.com>
2022-06-22 21:09:16 -0300

    header start structure

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar (){
+    return(
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Features</li>
+                <li>Pricing</li>
+                <li>Gallery</li>
+                <li>Team</li>
+                <li>Search</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/Layouts/Header.jsx
@@ -0,0 +1,14 @@
+import Navbar from '../Components/Navbar'
+
+function Header (){
+    return(
+        <header>
+            <div>
+                <p>icon</p>
+                <Navbar/>
+            </div>
+        </header>
+    )
+}
+
+export default Header
