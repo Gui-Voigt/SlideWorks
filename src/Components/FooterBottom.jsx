23c24a696740c885bffa6e3bbc534afe2fa0b7d1
Gui-Voigt <guilherme.voigt@outlook.com>
2022-06-24 10:48:16 -0300

    footer hiperlinks

--- src/Components/FooterBottom.jsx
+++ src/Components/FooterBottom.jsx
@@ -3,18 +3,23 @@ import face from '../Images/face.svg'
 import twitter from '../Images/twitter.svg'
 import instagram from '../Images/instagram.svg'
 
+const facelink = "https://www.facebook.com/profile.php?id=100011160456845"
+const twitterlink = "https://twitter.com/"
+const instalink = "https://www.instagram.com/g_voigt/"
+
+
 function FooterBottom (){
     return(
         <div className={styles.footerBottom}>
             <div className={styles.footerLeft}>
-                <p> Terms &#38; Conditions </p>
-                <p> Privacy Policy </p>
+                <a href=''><p> Terms &#38; Conditions </p></a>
+                <a href=''><p> Privacy Policy </p></a>
             </div>
 
             <div className={styles.footerRight}>
-                <img src={face} alt="logotipo facebook" />
-                <img src={twitter} alt="logotipo twitter" />
-                <img src={instagram} alt="logotipo instagram" />
+                <a href={facelink} target="blank"><img src={face} alt="logotipo facebook" /></a>
+                <a href={twitterlink} target="blank"><img src={twitter} alt="logotipo twitter" /></a>
+                <a href={instalink} target="blank"><img src={instagram} alt="logotipo instagram" /></a>
             </div>
        </div>
     )
